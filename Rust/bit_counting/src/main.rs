cc358ae9369aa6927c5da378c29e0b8e72aac424
japerry911 <jack.e.perry@outlook.com>
2020-08-09 12:41:47 -0600

    Rust Bit Counting kata completed

--- Rust/bit_counting/src/main.rs
+++ Rust/bit_counting/src/main.rs
@@ -9,7 +9,27 @@ case
 
 fn main() {
     println!("2 = {}", count_bits(10));
+    println!("5 = {}", count_bits(1234));
 }
 
 fn count_bits(n:i64) -> u32 {
+    let mut current_power_of_two:i64 = 1;
+    let mut number_of_bits:u32 = 0;
+    let mut mut_n:i64 = n;
+
+    while current_power_of_two <= n {
+        current_power_of_two *= 2;
+    }
+    current_power_of_two /= 2;
+
+    while mut_n > 0 {
+        if current_power_of_two <= mut_n {
+            mut_n -= current_power_of_two;
+            number_of_bits += 1;
+        }
+
+        current_power_of_two /= 2;
+    }
+
+    number_of_bits
 }
