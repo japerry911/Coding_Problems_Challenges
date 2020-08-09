70f0a0dcf658cec5d23e712f561fd683796b4b30
japerry911 <jack.e.perry@outlook.com>
2020-08-09 12:26:06 -0600

    Rust Bit Counting kata set up

---
+++ Rust/bit_counting/src/main.rs
@@ -0,0 +1,15 @@
+/*
+---Bit Counting---
+Write a function that takes an integer as input, and returns the number of bits that are equal to
+one in the binary representation of that number. You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this
+case
+ */
+
+fn main() {
+    println!("2 = {}", count_bits(10));
+}
+
+fn count_bits(n:i64) -> u32 {
+}
