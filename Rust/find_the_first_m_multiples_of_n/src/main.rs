5af2e8e14077c6112d5c441d083c771c28969261
japerry911 <jack.e.perry@outlook.com>
2020-08-08 23:13:42 -0600

    Rust Find the First M Multiples of N kata set up

---
+++ Rust/find_the_first_m_multiples_of_n/src/main.rs
@@ -0,0 +1,20 @@
+/*
+---Find the First M Multiples of N---
+Implement a function, multiples(m, n), which returns an array of the first m multiples of the real
+number n. Assume that m is a positive integer.
+
+Ex.
+
+multiples(3, 5.0)
+should return
+
+[5.0, 10.0, 15.0]
+ */
+
+fn main() {
+    println("{:?}", multiples(3, 5.0));
+}
+
+fn multiples(m:i32, n:f64) -> Vec<f64> {
+    return Vec::new();
+}
