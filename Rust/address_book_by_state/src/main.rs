cc791e35b7f9fac0fda63b3002703e9d277fac31
japerry911 <jack.e.perry@outlook.com>
2020-08-10 10:21:15 -0600

    Rust - Address Book by State kata set up

---
+++ Rust/address_book_by_state/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let input1:&str = "John Daggett, 341 King Road, Plymouth MA\nAlice Ford, 22 East Broadway, \
+                        Richmond VA\nSal Carpenter, 73 6th Street, Boston MA";
+    println!("{}", by_state(input1));
+}
+fn by_state(str:&str) -> String {
+    "".to_string()
+}
