1520a0c142f37d8f6a8d50c811957c81f315f726
japerry911 <jack.e.perry@outlook.com>
2020-08-10 14:03:32 -0600

    Rust - Steps in Primes kata and tests completed

--- Rust/steps_in_prime/src/main.rs
+++ Rust/steps_in_prime/src/main.rs
@@ -45,11 +45,75 @@ For Go: nil slice is expected when there are no step between m and n. Example: s
  */
 
 fn main() {
-    println!("(101, 103) = {:?}", steps(2,100,110));
-    println!("(103, 107) = {:?}", steps(4,100,110));
-    println!("(30089, 30097) = {:?}", steps(8,30000,100000));
+    println!("(101, 103) = {:?}", step(2,100,110));
+    println!("(103, 107) = {:?}", step(4,100,110));
+    println!("(30089, 30097) = {:?}", step(8,30000,100000));
+    println!("(1321, 1373) = {:?}", step(52, 1300, 15000));
+    println!("(10000139, 10000141) = {:?}", step(2,10000000,11000000));
 }
 
-fn steps(g: i32, m: u64, n: u64) -> Option<(u64, u64)> {
-    return None;
+fn step(g: i32, m: u64, n: u64) -> Option<(u64, u64)> {
+    for i in m..(n - (g as u64)) {
+        if is_prime(i) && is_prime(i + (g as u64)) {
+            return Some((i, i + (g as u64)));
+        }
+    }
+    None
 }
+
+fn is_prime(x: u64) -> bool {
+    if x == 2 {
+        return true;
+    } else if x < 3 || (x % 2 == 0) || (x % 5 == 0) {
+        return false;
+    }
+    let limit:u64 = (x as f64).sqrt() as u64;
+    for i in 3..=limit {
+        if x % i == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+// Too Slow
+/*fn step(g: i32, m: u64, n: u64) -> Option<(u64, u64)> {
+    let primes:Vec<u64> = generate_primes(m, n);
+    let mut seeking_primes = HashSet::new();
+
+    for prime in &primes {
+        let current_search:u64 = *prime - (g as u64);
+
+        let result = seeking_primes.contains(&current_search);
+        if result {
+            return Some((current_search, *prime));
+        }
+
+        seeking_primes.insert(prime);
+    }
+
+    None
+}
+
+fn generate_primes(start: u64, end: u64) -> Vec<u64> {
+    let mut prime_vec:Vec<u64> = Vec::new();
+    let limit = (end as f64).sqrt() as u64;
+    let mut bool_vec:Vec<bool> = Vec::new();
+    for _ in 0..end {
+        bool_vec.push(true);
+    }
+    for i in 2..=limit {
+        if bool_vec[i as usize] {
+            for x in ((i * i)..end).step_by(i as usize) {
+                bool_vec[x as usize] = false;
+            }
+        }
+    }
+    for i in start..end {
+        if bool_vec[i as usize] {
+            prime_vec.push(i);
+        }
+    }
+
+    prime_vec
+}*/
