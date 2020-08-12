8b89c3c7a7188c6b94ae9fdfe5ea908eb304332b
japerry911 <jack.e.perry@outlook.com>
2020-08-12 12:19:05 -0600

    cleaned up rental_car_cost solution

--- Elixir/transportation_on_vacation/lib/transportation_on_vacation.ex
+++ Elixir/transportation_on_vacation/lib/transportation_on_vacation.ex
@@ -2,16 +2,26 @@ defmodule TransportationOnVacation do
   def rental_car_cost(d) do
     cost = d * 40
 
-    if d >= 7 do
-      cost = cost - 50
-      cost
-    else
-      if d >= 3 do
-        cost = cost - 20
-        cost
-      else
-        cost
-      end
+    case d do
+      d when d >= 7 -> cost - 50
+      d when d >= 3 -> cost - 20
+      _ -> cost
     end
   end
+
+  # def rental_car_cost(d) do
+  #   cost = d * 40
+
+  #   if d >= 7 do
+  #     cost = cost - 50
+  #     cost
+  #   else
+  #     if d >= 3 do
+  #       cost = cost - 20
+  #       cost
+  #     else
+  #       cost
+  #     end
+  #   end
+  # end
 end
