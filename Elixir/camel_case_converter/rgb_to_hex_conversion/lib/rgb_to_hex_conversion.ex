0ea1e6e33b8ca6d1f3ded62679bbe059c848ef3c
japerry911 <jack.e.perry@outlook.com>
2020-08-13 14:12:07 -0600

    Elixir - RGB to Hex Conversion kata and tests completed

--- Elixir/camel_case_converter/rgb_to_hex_conversion/lib/rgb_to_hex_conversion.ex
+++ Elixir/camel_case_converter/rgb_to_hex_conversion/lib/rgb_to_hex_conversion.ex
@@ -1,5 +1,14 @@
 defmodule RgbToHexConversion do
   def rgb(r, g, b) do
-    r + g + b
+    convert_to_hex(r) <> convert_to_hex(g) <> convert_to_hex(b)
+  end
+
+  def convert_to_hex(num) do
+    case num do
+      x when x >= 255 -> "FF"
+      x when x <= 0 -> "00"
+      x when x <= 15 -> "0" <> Integer.to_string(x, 16)
+      x -> Integer.to_string(x, 16)
+    end
   end
 end
