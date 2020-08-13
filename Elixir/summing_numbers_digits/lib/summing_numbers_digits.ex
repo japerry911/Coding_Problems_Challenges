284fe3717f56d51ef95440485c238a5fa828075e
japerry911 <jack.e.perry@outlook.com>
2020-08-13 10:08:53 -0600

    Elixir - Summing Numbers Digits kata and tests completed

--- Elixir/summing_numbers_digits/lib/summing_numbers_digits.ex
+++ Elixir/summing_numbers_digits/lib/summing_numbers_digits.ex
@@ -1,5 +1,9 @@
 defmodule SummingNumbersDigits do
   def sum_digits(number) do
-    number
+    case number do
+      0 -> 0
+      x when x > 0 -> rem(number, 10) + sum_digits(div(number, 10))
+      x when x < 0 -> -rem(number, 10) + sum_digits(div(-number, 10))
+    end
   end
 end
