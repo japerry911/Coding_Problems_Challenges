48308ea1e7ead70519a0ee6291a0861176ed09cc
japerry911 <jack.e.perry@outlook.com>
2020-08-13 10:00:28 -0600

    Elixir - Fill the Hard Drive kata and tests completed

--- Elixir/fill_the_hard_drive/lib/fill_the_hard_drive.ex
+++ Elixir/fill_the_hard_drive/lib/fill_the_hard_drive.ex
@@ -1,5 +1,20 @@
 defmodule FillTheHardDrive do
   def save(sizes, hd) do
-    {sizes, hd}
+    {result_list, _space_taken} =
+      sizes
+      |> Enum.map_reduce({0, false}, fn x, acc ->
+        if elem(acc, 0) + x <= hd and elem(acc, 1) === false do
+          new_acc_value = elem(acc, 0) + x
+          acc = put_elem(acc, 0, new_acc_value)
+          {x, acc}
+        else
+          acc = put_elem(acc, 1, true)
+          {nil, acc}
+        end
+      end)
+
+    result_list
+    |> Enum.filter(fn x -> x != nil end)
+    |> length()
   end
 end
