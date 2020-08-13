e56509a1be631574bb92e44b814dfe145259d55d
japerry911 <jack.e.perry@outlook.com>
2020-08-13 12:28:23 -0600

    Elixir - Strongest Even Number kata and tests completed

--- Elixir/strongest_even_number/lib/strongest_even_number.ex
+++ Elixir/strongest_even_number/lib/strongest_even_number.ex
@@ -1,5 +1,40 @@
 defmodule StrongestEvenNumber do
   def strongest_even(n, m) do
-    n + m
+    calc_possible_strength(1, n, m, 1)
   end
+
+  def calc_possible_strength(multiple, min, max, starting_value) do
+    multiple = multiple * 2
+
+    if multiple > max do
+      calc_possible_strength(starting_value + 1, min, max, starting_value + 1)
+    else
+      if multiple < min do
+        calc_possible_strength(multiple, min, max, starting_value)
+      else
+        if multiple * 2 < max do
+          calc_possible_strength(multiple, min, max, starting_value)
+        else
+          multiple
+        end
+      end
+    end
+  end
+
+  # def strongest_even(n, m) do
+  #   {value, _strength} =
+  #     n..m
+  #     |> Enum.to_list()
+  #     |> Enum.map(fn x -> {x, calc_strength(x)} end)
+  #     |> Enum.max_by(fn {_value, strength} -> strength end)
+
+  #   value
+  # end
+
+  # def calc_strength(number) do
+  #   case number do
+  #     n when rem(n, 2) > 0 -> 0
+  #     n -> 1 + calc_strength(div(n, 2))
+  #   end
+  # end
 end
--- Elixir/strongest_even_number/test/strongest_even_number_test.exs
+++ Elixir/strongest_even_number/test/strongest_even_number_test.exs
@@ -17,4 +17,12 @@ defmodule StrongestEvenNumberTest do
   test "sample test 4" do
     assert StrongestEvenNumber.strongest_even(129, 193) === 192
   end
+
+  test "sample test 5" do
+    assert StrongestEvenNumber.strongest_even(40_835_279, 376_975_813) === 268_435_456
+  end
+
+  test "sample test 6" do
+    assert StrongestEvenNumber.strongest_even(3, 310) === 256
+  end
 end
