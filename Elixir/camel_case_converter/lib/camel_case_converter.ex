8cca2cd239b68b3f4d30a1573f02ca0d4f1621a5
japerry911 <jack.e.perry@outlook.com>
2020-08-13 13:44:53 -0600

    Elixir - Camel Case Converter kata and tests completed

--- Elixir/camel_case_converter/lib/camel_case_converter.ex
+++ Elixir/camel_case_converter/lib/camel_case_converter.ex
@@ -1,5 +1,27 @@
 defmodule CamelCaseConverter do
   def to_camel_case(str) do
-    str
+    if String.contains?(str, "-") do
+      String.split(str, "-")
+      |> Enum.with_index()
+      |> Enum.map(fn {x, i} ->
+        if i === 0 do
+          x
+        else
+          String.capitalize(x)
+        end
+      end)
+      |> Enum.join("")
+    else
+      String.split(str, "_")
+      |> Enum.with_index()
+      |> Enum.map(fn {x, i} ->
+        if i === 0 do
+          x
+        else
+          String.capitalize(x)
+        end
+      end)
+      |> Enum.join("")
+    end
   end
 end
