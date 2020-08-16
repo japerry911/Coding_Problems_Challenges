912780ab63f0c34fba8669c82d48c01edd06ce11
japerry911 <jack.e.perry@outlook.com>
2020-08-16 08:47:43 -0600

    Elixir - Split Strings kata and tests completed

--- Elixir/split_strings/lib/split_strings.ex
+++ Elixir/split_strings/lib/split_strings.ex
@@ -1,5 +1,18 @@
 defmodule SplitStrings do
   def solution(str) do
     str
+    |> String.to_charlist()
+    |> Enum.to_list()
+    |> split_string
+    |> Enum.map(fn x -> List.to_string(x) end)
+  end
+
+  def split_string(str) do
+    if rem(length(str), 2) == 0 do
+      Enum.chunk_every(str, 2)
+    else
+      str = str ++ '_'
+      Enum.chunk_every(str, 2)
+    end
   end
 end
