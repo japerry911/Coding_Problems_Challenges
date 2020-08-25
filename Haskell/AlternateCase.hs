3dfb39b471fcc960a49bf3837f324cdb1e85b2f2
japerry911 <jack.e.perry@outlook.com>
2020-08-25 14:37:56 -0600

    Haskell - AlternateCase kata set up'

---
+++ Haskell/AlternateCase.hs
@@ -0,0 +1,10 @@
+{-
+---Alternate Case---
+Write function alternateCase which switch every letter in string from 
+upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
+-}
+
+main = print $ alternateCase "Hello World"
+
+alternateCase :: String -> String
+alternateCase s = s
